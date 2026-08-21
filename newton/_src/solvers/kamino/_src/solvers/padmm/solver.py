92852b7606cd181fd29b2a263c13bfbbe01ad5cd
Tomas Thoresen <tomasthoresen@gmail.com>
2026-08-21 09:25:15 +0200

    Warn when platform support disables PADMM graph conditionals
    Signed-off-by: Tomas Thoresen <tomasthoresen@gmail.com>

--- newton/_src/solvers/kamino/_src/solvers/padmm/solver.py
+++ newton/_src/solvers/kamino/_src/solvers/padmm/solver.py
@@ -243,6 +243,11 @@ class PADMMSolver:
         self._warmstart = warmstart
         self._use_acceleration = use_acceleration
         self._use_graph_conditionals = use_graph_conditionals and wp.is_conditional_graph_supported()
+        if use_graph_conditionals and not self._use_graph_conditionals:
+            msg.warning(
+                "Graph conditionals are unavailable on this platform, "
+                "using unrolled for-loops over max iterations in PADMM solver."
+            )
         self._collect_info = collect_info
 
         # Check if any world uses adaptive penalty updates (requiring per-step regularization updates)
